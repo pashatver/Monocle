f17d18f0ce6e770c5f74778f4926b67ae0b3cd1c
Pasha_T <pashatver@mail.ru>
2022-04-03 13:53:04 +0300

    ver 0.3

--- src/Game.java
+++ src/Game.java
@@ -1,36 +1,37 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
     private static int PLAYERS_NUMBER;
-    private Player[] playersArray;
+    private ArrayList<Player> playersList;
 
     public void playerGenerator(){
         Scanner input = new Scanner(System.in);
         System.out.println("Введите число игроков: ");
         PLAYERS_NUMBER = input.nextInt();
-        playersArray = new Player [PLAYERS_NUMBER];
+        playersList = new ArrayList<>();
 
         for (int i = 0; i < PLAYERS_NUMBER; i++) {
             Scanner nameInput = new Scanner(System.in);
             System.out.printf("Введите имя %d-го игрока: ", i + 1);
             String playerName = nameInput.nextLine();
-            playersArray[i] = new Player(playerName, 500);
+            playersList.add(new Player(playerName, 500));
         }
     }
 
     public void printPlayersNames() {
         System.out.print("Вот их имена: ");
         for (Player player:
-             playersArray) {
+                playersList) {
             System.out.print(player.getPlayerName() + " ");
         }
         System.out.println();
     }
 
-    public Player[] getPlayersArray() {
-        return playersArray;
+    public ArrayList<Player> getPlayersList() {
+        return playersList;
     }
     public Player getPlayer (int number) {
-        return playersArray[number];
+        return playersList.get(number);
     }
 }
